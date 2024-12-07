a752e5f059baf54170247f24e75e64a8f3a95bfa
Khalid Azmatullah <khalid.studymaterial@gmail.com>
2024-12-07 18:09:40 +0530

    proto2

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -2,10 +2,23 @@ window.onload = function() {
   const Params = new URLSearchParams(window.location.search);
   const Name = Params.get('user');
 
-  document.getElementById('nameArea').textContent = Name;
   const NameFadeIn = document.getElementById('nameArea');
+  const EarlyViewMessage = document.getElementById('earlyView');
+  EarlyViewMessage.style.opacity=0;
+  NameFadeIn.style.opacity =0;
+  document.getElementById('nameArea').textContent = Name;
   NameFadeIn.classList.remove('welcomeBox')
+  EarlyViewMessage.classList.remove('shine')
   setTimeout(() => {
-    NameFadeIn.classList.add('welcomeBox');
+    NameFadeIn.style.opacity=1
+    NameFadeIn.classList.add('welcomeBox'); 
   }, 2000);
+  const FirstFadeOut = document.getElementById('firstView')
+  setTimeout(() => {
+    EarlyViewMessage.style.opacity=1
+    EarlyViewMessage.classList.add('shine')
+    FirstFadeOut.classList.add('fadeOutUp')
+  }, 4500);
+  EarlyViewMessage.classList.remove('shine')
+  EarlyViewMessage.classList.add('startShine')
 };
